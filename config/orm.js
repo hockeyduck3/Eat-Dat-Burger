7bcf9a33d1876149d47bfc499f3852fc19be3623
LJ Spencer <hockeyduck3@gmail.com>
2020-05-28 20:03:20 -0600

    Added some empty functions for the orm.js file

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const orm = {
+    selectAll: function () {
+        
+    },
+
+    insertOne: function() {
+
+    },
+
+    updateOne: function() {
+
+    }
+}
+
+module.exports = orm;
