fd90200b35c7dc3d2d46403169e9e71fd4b20703
LJ Spencer <hockeyduck3@gmail.com>
2020-05-28 20:10:45 -0600

    Added some starter code for the three functions in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,46 @@
 const connection = require('./connection');
 
 const orm = {
-    selectAll: function () {
-        
+    selectAll: function (cb) {
+        let querySearch = 'SELECT * FROM burgers';
+
+        connection.query(
+            querySearch,
+
+            function (err, data) {
+                if (err) throw err;
+
+                cb(data);
+            }
+        );
     },
 
-    insertOne: function() {
+    insertOne: function(table, name, cb) {
+        let querySearch = `INSERT INTO ${table} VALUES(${name}, false)`;
 
+        connection.query(
+            querySearch,
+            
+            function(err, data) {
+                if (err) throw err;
+
+                cb(data);
+            }
+        );
     },
 
-    updateOne: function() {
+    updateOne: function(table, where, id, cb) {
+        let querySearch = `UPDATE ${table} WHERE ${where} = ${id}`;
 
+        connection.query(
+            querySearch,
+
+            function(err, data) {
+                if (err) throw err;
+    
+                cb(data);
+            }
+        );
     }
 }
 
