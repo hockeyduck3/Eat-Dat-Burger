2911ea8dcba64ecbe2922cc8e387f938c82eac79
LJ Spencer <hockeyduck3@gmail.com>
2020-05-29 20:07:19 -0600

    Added some code to the burger.js file

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,23 @@
 const orm = require('../config/orm');
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll(data => {
+            cb(data)
+        });
+    },
+
+    insertOne: function(burgers, nameVal, cb) {
+        orm.insertOne(burgers, nameVal, function(res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function(burgers, whereVal, idVal, cb) {
+        orm.updateOne(burgers, whereVal, id, function(results) {
+            cb(results);
+        });
+    }
+}
+
+module.exports = burger;
