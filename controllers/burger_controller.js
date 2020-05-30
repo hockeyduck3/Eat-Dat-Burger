1a09363e6a77d4141a0e3478c72f14c16746d57a
LJ Spencer <hockeyduck3@gmail.com>
2020-05-29 20:32:20 -0600

    Added some more code to the burger_controller file

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,2 +1,44 @@
 const express = require('express');
+const router = express.Router();
+
 const burger = require('../models/burger');
+
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        let object = {
+            burgers: data
+        };
+        
+        res.render('index', object);
+    });
+});
+
+router.post('/burger', function(req, res) {
+    burger.insertOne(
+    [
+        'name'
+    ], 
+    
+    [
+        req.body.name
+    ], 
+    
+    function(result) {
+        console.log(result);
+    });
+});
+
+router.put('/burgers/:id', function(req, res) {
+    let condition = `id = ${req.params.id}`;
+
+    burger.updateOne(
+    {
+        devoured: true
+    }, 
+    
+    condition, function(data) {
+        console.log(data);
+    });
+});
+
+module.exports = router;
